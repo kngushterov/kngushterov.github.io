db893b739f8bb706d1e4a3200bf47a46c06b12f1
konstantin.gushterov <konstantin.gushterov@lights.digital>
2023-09-13 16:45:36 +0300

    feat: create footer component

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex justify-end gap-6 bg-primary-800 text-white p-6 w-4/5">
+    <img class="h-16 w-16 self-center" src="../../public/email.svg" />
+    <div class="flex flex-col gap-2">
+      <a class="text-2xl font-bold italic underline" href="mailto:konstantin.gushterov@gmail.com"
+        >konstantin.gushterov@gmail.com</a
+      >
+      <a
+        class="text-2xl font-bold italic underline"
+        target="_blank"
+        href="https://www.linkedin.com/in/konstantin-gushterov/"
+        >linkedin.com/in/konstantin-gushterov</a
+      >
+    </div>
+  </div>
+</template>
