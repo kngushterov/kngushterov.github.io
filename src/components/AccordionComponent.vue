280bf4dea4766776e0cfbcd2e6d3943dfc60534e
konstantin.gushterov <konstantin.gushterov@lights.digital>
2023-09-13 16:45:53 +0300

    feat: create accordion component

---
+++ src/components/AccordionComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div
+      v-on:click="isOpen = !isOpen"
+      class="flex justify-between font-bold text-2xl text-white border-2 bg-primary px-4 py-2 rounded-lg cursor-pointer"
+    >
+      <header class="italic uppercase">
+        {{ title }}
+      </header>
+      <img class="w-6 h-6 self-center" src="../../public/accordion-arrow.svg" />
+    </div>
+    <div :class="!isOpen && 'hidden'">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  data: () => {
+    return {
+      isOpen: true
+    }
+  },
+  name: 'accordion-component',
+  props: {
+    title: String
+  }
+}
+</script>
