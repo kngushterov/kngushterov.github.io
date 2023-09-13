7fe5d30a95db103c91fd0f453bbb556de310ac47
konstantin.gushterov <konstantin.gushterov@lights.digital>
2023-09-13 16:46:37 +0300

    feat: create time-and-location component

---
+++ src/components/TimeAndLocation.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="flex gap-8">
+    <div :class="divStyles">
+      <img :class="imgStyles" src="../../public/calendar.svg" />
+      <p :class="pStyles">{{ timeInterval.from }} - {{ timeInterval.to }}</p>
+    </div>
+    <div :class="divStyles">
+      <img :class="imgStyles" src="../../public/location.svg" />
+      <p :class="pStyles">{{ location }}</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  data: () => {
+    return {
+      divStyles: 'flex gap-1',
+      imgStyles: 'h-3 w-3 self-center',
+      pStyles: 'text-sm text-neutral'
+    }
+  },
+  name: 'time-and-location',
+  props: {
+    timeInterval: {
+      default: {
+        from: '',
+        to: ''
+      }
+    },
+    location: String
+  }
+}
+</script>
