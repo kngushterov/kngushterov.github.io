3294497ed1b73a237d2cdd780456f59ab6701b21
konstantin.gushterov <konstantin.gushterov@lights.digital>
2023-09-13 16:47:28 +0300

    style: improve header component

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="bg-sky-800 text-white mt-10 ml-8 w-4/5">
-    <p class="text-3xl font-bold underline">Konstantin Gushterov</p>
-    <p class="text-xl">Software Developer</p>
+  <div class="bg-primary-800 text-white p-6 w-4/5">
+    <p class="text-3xl font-bold">Konstantin Gushterov</p>
+    <p class="text-2xl font-bold text-secondary">Web Developer</p>
   </div>
 </template>
