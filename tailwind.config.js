4f20bcb69ae7487fd7854afa4487196889ebe962
konstantin.gushterov <konstantin.gushterov@lights.digital>
2023-09-13 14:53:24 +0300

    style: add theme colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
+import colors from 'tailwindcss/colors'
 /** @type {import('tailwindcss').Config} */
 // eslint-disable-next-line no-undef
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: colors.sky['800'],
+        secondary: colors.amber['600']
+      }
+    }
   },
   plugins: []
 }
