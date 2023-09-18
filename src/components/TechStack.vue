e5ba26f1d9dcc469cc3e69c37b2515cdb631acd7
konstantin.gushterov <konstantin.gushterov@lights.digital>
2023-09-18 14:42:20 +0300

    feat: create tech-stack component

---
+++ src/components/TechStack.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="columns-2 space-y-3 xl:columns-3">
+    <p
+      v-for="(tech, i) in techStack"
+      :key="i"
+      class="border-2 border-secondary font-bold text-primary h-fit rounded text-center"
+    >
+      {{ tech }}
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import { techStack } from '@/projects'
+
+export default {
+  name: 'tech-stack',
+  data: () => {
+    return { techStack }
+  }
+}
+</script>
