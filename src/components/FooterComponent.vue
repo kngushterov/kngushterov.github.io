c260361c419dff387369c5689d9ffa525d98f8ad
konstantin.gushterov <konstantin.gushterov@lights.digital>
2023-09-18 16:52:35 +0300

    style: improve header and footer for phones

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex justify-end gap-6 bg-primary-800 text-white p-6 w-4/5">
+  <div class="flex justify-end gap-3 md:gap-6 bg-primary-800 text-white p-3 md:p-6 w-4/5">
     <img class="h-12 w-12 md:h-16 md:w-16 self-center" src="/email.svg" />
     <div class="flex flex-col gap-2">
       <a
--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="bg-primary-800 text-white p-6 w-4/5">
-    <p class="text-3xl font-bold">Konstantin Gushterov</p>
-    <p class="text-2xl font-bold text-secondary">Web Developer</p>
+  <div class="bg-primary-800 text-white p-3 sm:p-6 w-4/5">
+    <p class="text-xl sm:text-3xl font-bold">Konstantin Gushterov</p>
+    <p class="text-lg sm:text-2xl font-bold text-secondary">Web Developer</p>
   </div>
 </template>
