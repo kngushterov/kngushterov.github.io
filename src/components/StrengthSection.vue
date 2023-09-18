20b29c46b5e9217b698e19a084d3b1d28a5a4d13
konstantin.gushterov <konstantin.gushterov@lights.digital>
2023-09-18 14:39:21 +0300

    feat: create strength-section component

---
+++ src/components/StrengthSection.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="flex gap-2 items-center">
+      <img class="h-8 w-8" :src="`/${icon}.svg`" />
+      <header class="font-bold text-xl">
+        {{ title }}
+      </header>
+    </div>
+    <p>
+      {{ description }}
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'strength-section',
+  props: {
+    icon: String,
+    title: String,
+    description: String
+  }
+}
+</script>
---
+++ src/strengths.ts
@@ -0,0 +1,26 @@
+export interface Strength {
+  icon: string
+  title: string
+  description: string
+}
+
+export const strengths: Strength[] = [
+  {
+    icon: 'progress',
+    title: 'Life as a RPG Game (love those!)',
+    description:
+      'It brings me sheer excitement to see myself being better than I was yesterday and to know that now I am prepared to take on challenges (love those as well!) that would have probably overwhelmed me in the past. And, of course, every game is way better with friends!'
+  },
+  {
+    icon: 'motivation',
+    title: 'My Word is My Bond (and I use it to my advantage)',
+    description:
+      'I often publicly make bold declarations and by doing that I leave myself no choice but to do everything necessary to follow through on them. That works like a charm because it keeps me fully motivated to get the job done.'
+  },
+  {
+    icon: 'consistency',
+    title: 'Call Me Consistency',
+    description:
+      'My name is of Latin origin and means "constant, steadfast". I love it and if I can\'t live up to it, I\'d have to change it, which is simply not happening.'
+  }
+]
