6cce82520832a36521a6251cff9a34bd61f898eb
Konstantin Gushterov <konstantin.gushterov@gmail.com>
2025-04-04 17:29:30 +0300

    feat: add profile picture in header

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,8 +1,15 @@
 <template>
-  <div class="bg-primary-800 text-white p-3 sm:p-6 w-4/5">
-    <p class="text-xl sm:text-3xl font-bold">Konstantin Gushterov</p>
-    <p class="text-lg sm:text-2xl font-bold text-secondary">
-      Full Stack Web Developer - Mid/Senior Level
-    </p>
+  <div class="flex w-4/5 xl:px-12 xl:pt-3 items-center">
+    <img
+      src="/cv-photo.png"
+      alt="Profile Picture"
+      class="w-24 h-24 xl:w-40 xl:h-40 rounded-3xl border-4 border-white xl:-mb-9 xl:relative z-20"
+    />
+    <div class="bg-primary-800 text-white p-3 sm:p-6">
+      <p class="text-xl sm:text-3xl font-bold">Konstantin Gushterov</p>
+      <p class="text-lg sm:text-2xl font-bold text-secondary">
+        Full Stack Web Developer - Mid/Senior Level
+      </p>
+    </div>
   </div>
 </template>
