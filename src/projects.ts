ba612293a3acfe79bf8fa19b0597e2c0a333f96e
Konstantin Gushterov <konstantin.gushterov@gmail.com>
2025-04-06 19:34:28 +0300

    update: add creativity strength

--- src/projects.ts
+++ src/projects.ts
@@ -21,7 +21,7 @@ export const projects: Project[] = [
     description:
       "A set of tools designed to streamline the processes of internal management, storing, distribution, testing and analyzing of software created for BMW Group's vehicles",
     points: [
-      'Technologies: TypeScript, React, React Query, MaterialUI, Express, MongoDB, Mongoose, Jest, Git, Jira',
+      'Technologies: TypeScript, React, React Query, MaterialUI, Express, MongoDB, Mongoose, Jest, React Testing Library, Vitest, Git, Jira',
       'I was the Development Lead in this project. Some of my responsibilities were to come up with feature ideas, designs and architecture of new tools.',
       'Actively participated in implementing best practices for project management, code quality and performance.',
       'Onboarded and guided 2 colleagues when they joined the project.'
@@ -37,7 +37,7 @@ export const projects: Project[] = [
     description:
       '8 unique web apps found in the Infotainment systems of various models from VW and Skoda',
     points: [
-      'Technologies: TypeScript, Angular 2+, RxJS, SCSS, Express, Jasmine + Karma, proprietary VW frameworks, Git, Jira',
+      'Technologies: TypeScript, Angular 2+, RxJS, SCSS, Express, Jasmine, Karma, Cypress, proprietary VW frameworks, Git, Jira',
       'Learned the Agile principles and Scrum framework using Jira for Project Management',
       'Worked in an international team of around 30 people covering all roles of the software development process',
       'Onboarded 3 junior colleagues into the project and mentored them until their promotion to mid-level developers'
@@ -47,13 +47,13 @@ export const projects: Project[] = [
     title: 'Various Practice Projects',
     timeInterval: {
       from: '06/2021',
-      to: 'present'
+      to: 'Present'
     },
     description:
       'Side projects to practice, experiment and learn new technologies, as well as master the ones I am using in production',
     points: [
-      'Front-end: Vue, Preact, Tailwind CSS, Redux Toolkit, NgRx',
-      'Back-end and DB: NestJS, Fastify, PostgreSQL, Prisma',
+      'Front-end: Vue, Preact, Tailwind CSS, Shadcn, Redux Toolkit, NgRx',
+      'Back-end & DB: NestJS, Fastify, PostgreSQL, Prisma',
       'Authentication: JWT, Auth0',
       'CI/CD: Docker, NGINX, Azure App Service',
       'Mobile: Flutter, Dart',
@@ -67,14 +67,22 @@ export const prodTechStack: string[] = [
   'Express',
   'MongoDB',
   'Mongoose',
+
+  'Vite',
   'React',
   'React Query',
   'Angular 2+',
   'RxJS',
   'SCSS',
   'MaterialUI',
+
   'Jest',
+  'Vitest',
+  'React Testing Library',
   'Jasmine',
+  'Karma',
+  'Cypress',
+
   'Git',
   'Jira'
 ]
@@ -84,16 +92,21 @@ export const upskillTechStack: string[] = [
   'Fastify',
   'PostgreSQL',
   'Prisma',
+
   'Vue',
   'Preact',
   'Tailwind CSS',
+  'Shadcn',
   'Redux Toolkit',
   'NgRx',
+
   'JWT',
   'Auth0',
+
   'Docker',
   'NGINX',
   'Azure App Service',
+
   'Flutter',
   'Dart'
 ]
--- src/strengths.ts
+++ src/strengths.ts
@@ -12,13 +12,19 @@ export const strengths: Strength[] = [
       "I've never missed a deadline and I am always willing to put some extra hours in to make sure I deliver or exceed the expected quality. I love earning the respect of my colleagues and contributing to their and the company's success. I value honesty and encourage constructive feedback."
   },
   {
-    icon: 'motivation',
+    icon: 'consistency',
     title: 'Independent yet Dependable',
     description:
-      "I respect everyone's time and call for help only as a last resort. This not only makes me a better teammate, but also allows me to learn through trial and error and gain experience faster. At the same time I love helping others, which is why I keep returning voluntarily as a mentor in Telerik Academy."
+      "I respect everyone's time and call for help only as a last resort. This not only makes me a better teammate, but also allows me to learn through trial and error and gain experience faster. At the same time I love helping others, which is why I keep returning voluntarily as a mentor to current students in Telerik Academy."
   },
   {
-    icon: 'consistency',
+    icon: 'creativity',
+    title: 'Creative Problem Solver',
+    description:
+      'For me the best part of being a web developer is the opportunity to create positive experiences for people through beautiful and intuitive UI/UX designs. Another great one is the challenge of solving complex problems. I find it immensely satisfying to come up with creative solutions that are as elegant as they are effective.'
+  },
+  {
+    icon: 'motivation',
     title: 'Lawyer Turned Developer ',
     description:
       'Many people find it strange that I jumped from being a lawyer to being a developer. It turned out that some of the skills I honed back then translate beautifully: problem solving, decision making, communication with clients, time management, adaptability, acquiring new skills, and the best one - efficiently finding, understanding and putting to use relevant new information.'
